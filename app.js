b6cc6f3a28f7fc5844f058d8e731ca45a74f0a3c
Anh Phuong La Russo <aphuong85@gmail.com>
2014-08-26 22:55:39 -0400

    Add new files

---
+++ app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+  $(".start_button").click(function() {
+    $(".intro_page").hide();
+    $(".question_1").show();
+  });
+
+  $(".submit_answer1").click(function() {
+    $(".question_1").hide();
+    $(".question_2").show();
+  });
+
+  $(".submit_answer2").click(function() {
+    $(".question_2").hide();
+    $(".question_3").show();
+  });
+
+  $(".submit_answer3").click(function() {
+    $(".question_3").hide();
+    $(".question_4").show();
+  });
+
+  $(".submit_answer4").click(function() {
+    $(".question_4").hide();
+    $(".question_5").show();
+  });
+
+});
